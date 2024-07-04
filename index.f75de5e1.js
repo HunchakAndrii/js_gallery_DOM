1739242e088738e292cb9a43aae40a61cc912194
HunchakAndrii <andreygunchak1@gmail.com>
2024-07-04 23:23:39 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const galleryList = document.querySelector(".gallery__list");
+const bigImg = document.querySelector(".gallery__large-img");
+// eslint-disable-next-line no-shadow
+galleryList.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const link = event.target.closest(".list-item__link");
+    if (!link) return;
+    bigImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
